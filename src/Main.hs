5e80799871e10668d09231bbb0b92c2b0d5edecc
Gabe Berke-Williams <gabebw@gabebw.com>
2015-12-10 12:48:52 -0800

    Open a websocket to Slack's real-time endpoint
    Use the higher level Wuss library to connect to a secure websocket.
    
    https://hackage.haskell.org/package/wuss

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,42 @@
 import System.Environment
 import GHC.Generics
+import Data.Maybe (fromJust)
+import Control.Monad (forever, void)
 import Control.Lens
-import Network.Wreq
 import Data.Aeson
+import Network.URI
+import Network.Wreq (getWith, asJSON, Response, defaults, param, responseBody)
+import Wuss
+import qualified Network.WebSockets as WS
 import qualified Data.Text as T
+
+app :: WS.ClientApp ()
+app connection = do
+    putStrLn "Connected!"
+
+    void . forever $ do
+        message <- WS.receiveData connection
+        print (message :: T.Text)
+
+    WS.sendClose connection (T.pack "Bye!")
 
 data RTMStartResponse = RTMStartResponse { url :: String }
   deriving (Generic, FromJSON, Show)
 
 rtmStartResponse :: T.Text -> IO (Response RTMStartResponse)
 rtmStartResponse token = asJSON =<< getWith opts "https://slack.com/api/rtm.start"
-  where
-    opts = defaults & param "token" .~ [token]
+    where
+        opts = defaults & param "token" .~ [token]
+
+runSecureClient' :: URI -> IO ()
+runSecureClient' uri = runSecureClient host 443 (uriPath uri) app
+    where
+        host = fromJust $ uriRegName <$> uriAuthority uri
 
 main :: IO ()
 main = do
-  token <- T.pack <$> getEnv "SLACK_API_TOKEN"
-  response <- rtmStartResponse token
-  print $ response ^. responseBody
+    token <- T.pack <$> getEnv "SLACK_API_TOKEN"
+    response <- rtmStartResponse token
+    let socketURL = url $ response ^. responseBody
+    let uri = fromJust $ parseURI socketURL
+    runSecureClient' uri

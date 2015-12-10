b5c12fb22d7e0a29699b6d14b3aebee2889ddf85
Tony DiPasquale <tony@thoughtbot.com>
2015-12-10 11:33:40 -0800

    Use wreq to call rtm.start on slack API

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,20 @@
-main = putStrLn "Hello"
+import System.Environment
+import GHC.Generics
+import Control.Lens
+import Network.Wreq
+import Data.Aeson
+import qualified Data.Text as T
+
+data RTMStartResponse = RTMStartResponse { url :: String }
+  deriving (Generic, FromJSON, Show)
+
+rtmStartResponse :: T.Text -> IO (Response RTMStartResponse)
+rtmStartResponse token = asJSON =<< getWith opts "https://slack.com/api/rtm.start"
+  where
+    opts = defaults & param "token" .~ [token]
+
+main :: IO ()
+main = do
+  token <- T.pack <$> getEnv "SLACK_API_TOKEN"
+  response <- rtmStartResponse token
+  print $ response ^. responseBody

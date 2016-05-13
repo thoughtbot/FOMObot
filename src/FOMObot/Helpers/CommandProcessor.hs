3fa6b6045b308613cf4169f6917c308096d75eef
Tony DiPasquale <tony@thoughtbot.com>
2016-05-13 14:28:02 -0700

    Add real help documentation

--- src/FOMObot/Helpers/CommandProcessor.hs
+++ src/FOMObot/Helpers/CommandProcessor.hs
@@ -2,7 +2,6 @@ module FOMObot.Helpers.CommandProcessor
     ( processCommand
     ) where
 
-import Control.Monad.IO.Class (liftIO)
 import Data.Monoid ((<>))
 import qualified Data.Text as T
 import qualified Web.Slack as Slack
@@ -19,10 +18,18 @@ processCommand (Slack.Message cid (Slack.UserComment uid) txt _ _ _) =
       (Remove xs) -> removeUserPrefs uid xs
       List -> Slack.sendMessage cid =<< (joinChannels <$> getUserPrefs uid)
       Stop -> deleteUserPrefs uid
-      Help -> liftIO $ print "help" -- still need to print help instructions
+      Help -> Slack.sendMessage cid helpText
       Unknown -> return ()
   where
     joinChannels [] = "No preferences set."
     joinChannels cids = "<#" <> T.intercalate "> <#" (map T.pack cids) <> ">"
 
 processCommand _ = return ()
+
+helpText :: T.Text
+helpText = "Possible Commands:\
+    \\nadd [#channel ...] : Add channels that you would like to monitor for activity.\
+    \\nremove [#channel ...] : Remove channels that you would no longer like to monitor.\
+    \\nlist : List the channels you are monitoring for activity.\
+    \\nstop : Stop FOMObot from monitoring any channels for activity.\
+    \\nhelp : Print the help text."
